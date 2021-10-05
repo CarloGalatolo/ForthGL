3649df8539fa1d5c516bfa952c0459776fd48c89
Salvatore Signorino <salvatore.signorino1701@gmail.com>
2021-10-05 15:48:32 +0200

    Commit suicide on Test 2, codice modificato

---
+++ Test2/Raspberry-Pi-master/GLES2/Build/emb-stdio.d
@@ -0,0 +1,8 @@
+Build/emb-stdio.o: \
+ /home/yoler/Scrivania/Raspberry-Pi-master/GLES2/emb-stdio.c \
+ /home/yoler/Scrivania/Raspberry-Pi-master/GLES2/rpi-smartstart.h \
+ /home/yoler/Scrivania/Raspberry-Pi-master/GLES2/emb-stdio.h
+
+/home/yoler/Scrivania/Raspberry-Pi-master/GLES2/rpi-smartstart.h:
+
+/home/yoler/Scrivania/Raspberry-Pi-master/GLES2/emb-stdio.h:
---
+++ Test2/Raspberry-Pi-master/GLES2/Build/rpi-SmartStart.d
@@ -0,0 +1,8 @@
+Build/rpi-SmartStart.o: \
+ /home/yoler/Scrivania/Raspberry-Pi-master/GLES2/rpi-SmartStart.c \
+ /home/yoler/Scrivania/Raspberry-Pi-master/GLES2/Font8x16.h \
+ /home/yoler/Scrivania/Raspberry-Pi-master/GLES2/rpi-smartstart.h
+
+/home/yoler/Scrivania/Raspberry-Pi-master/GLES2/Font8x16.h:
+
+/home/yoler/Scrivania/Raspberry-Pi-master/GLES2/rpi-smartstart.h:
